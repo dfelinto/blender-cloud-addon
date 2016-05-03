329d830f634966d874aeb39ee855e03ab231c2fe
Sybren A. Stüvel <sybren@stuvel.eu>
2016-05-03 13:41:44 +0200

    Demoted version to 0.9.0, to give us some incremental releases before 1.0

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ setup(
               'wheels': BuildWheels},
     name='blender_cloud',
     description='The Blender Cloud addon allows browsing the Blender Cloud from Blender.',
-    version='1.0.0',
+    version='0.9.0',
     author='Sybren A. Stüvel',
     author_email='sybren@stuvel.eu',
     packages=find_packages('.'),
