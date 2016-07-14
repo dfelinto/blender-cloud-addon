53ab2fc6dfbcb2f6c1e7eb30cd1f20b55f03e9f9
Sybren A. Stüvel <sybren@stuvel.eu>
2016-07-14 11:50:19 +0200

    Bumped version to 1.3.1

--- blender_cloud/__init__.py
+++ blender_cloud/__init__.py
@@ -21,7 +21,7 @@
 bl_info = {
     'name': 'Blender Cloud',
     'author': 'Sybren A. Stüvel and Francesco Siddi',
-    'version': (1, 3, 0),
+    'version': (1, 3, 1),
     'blender': (2, 77, 0),
     'location': 'Addon Preferences panel, and Ctrl+Shift+Alt+A anywhere for texture browser',
     'description': 'Texture library browser and Blender Sync. Requires the Blender ID addon '
